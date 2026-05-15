56a29854d75dbd9efef7e7d27e3c4c71217f3730
mtnakada <marshall.nakada@gmail.com>
2026-05-14 18:57:28 -0700

    functional screen demo

--- SequencePOC/src/main.cpp
+++ SequencePOC/src/main.cpp
@@ -8,6 +8,7 @@
  ****************************************************/
 
 #include "Adafruit_ThinkInk.h"
+#include <SoftwareSPI.h>
 #define EPD_DC 15
 #define EPD_CS 17
 #define EPD_BUSY 12 // can set to -1 to not use a pin (will wait a fixed delay)
@@ -15,7 +16,15 @@
 #define EPD_RESET 13  // can set to -1 and share with microcontroller Reset!
 #define EPD_SPI SPI // primary SPI
 
+// #define SPI1_CLK 6
+// #define SPI1_SDO 7
+// #define SPI1_SDI 8
+
+// SoftwareSPI soft_spi1(SPI1_CLK, SPI1_SDI, SPI1_SDO);
+
+
 // 3.7" Tricolor Display with 420x240 pixels and UC8253 chipset
+// ThinkInk_370_Tricolor_BABMFGNR display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, &soft_spi1);
 ThinkInk_370_Tricolor_BABMFGNR display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 
 // Function declaration
