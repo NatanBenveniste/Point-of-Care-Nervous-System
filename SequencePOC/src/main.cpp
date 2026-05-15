691d1d8df73e315034b6d37d5de8705a743a1b5f
mtnakada <marshall.nakada@gmail.com>
2026-05-15 10:19:12 -0700

    update: test SPI switching on Pi

--- SequencePOC/src/main.cpp
+++ SequencePOC/src/main.cpp
@@ -1,113 +1,76 @@
-/***************************************************
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LPS35HW.h>
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
+#define CS_UPSTREAM 17
+#define CS_DOWNSTREAM 5
 
-#include "Adafruit_ThinkInk.h"
-#define EPD_DC 15
-#define EPD_CS 17
-#define EPD_BUSY 12 // can set to -1 to not use a pin (will wait a fixed delay)
-#define SRAM_CS 14
-#define EPD_RESET 13  // can set to -1 and share with microcontroller Reset!
-#define EPD_SPI SPI // primary SPI
+Adafruit_LPS35HW upstreamSensor = Adafruit_LPS35HW();
+Adafruit_LPS35HW downstreamSensor = Adafruit_LPS35HW();
 
-// 3.7" Tricolor Display with 420x240 pixels and UC8253 chipset
-ThinkInk_370_Tricolor_BABMFGNR display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+float zeroOffset_Pa = 0.0;
 
-// Function declaration
-void testdrawtext(const char *text, uint16_t color);
+float readUpstreamPa() {
+  return upstreamSensor.readPressure() * 100.0;
+}
+
+float readDownstreamPa() {
+  return downstreamSensor.readPressure() * 100.0;
+}
 
 void setup() {
   Serial.begin(115200);
+
+  delay(3000);
+
+  Serial.println("serial");
+
+  if (!upstreamSensor.begin_SPI(CS_UPSTREAM)) {
+    Serial.println("UPSTREAM_SENSOR_INIT_FAILED");
+    while (1);
+  }
+
+  if (!downstreamSensor.begin_SPI(CS_DOWNSTREAM)) {
+    Serial.println("DOWNSTREAM_SENSOR_INIT_FAILED");
+    while (1);
+  }
+
+
+
   delay(1000);
 
-  Serial.println("Starting");
+  float sum = 0.0;
+  int samples = 100;
 
-  pinMode(EPD_BUSY, INPUT);
+  for (int i = 0; i < samples; i++) {
+    float upstream_Pa = readUpstreamPa();
+    float downstream_Pa = readDownstreamPa();
 
-  Serial.print("BUSY pin state: ");
-  Serial.println(digitalRead(EPD_BUSY));
+    sum += upstream_Pa - downstream_Pa;
+    delay(20);
+  }
 
-  display.begin(THINKINK_TRICOLOR);
+  zeroOffset_Pa = sum / samples;
 
-  Serial.println("Display begin done");
-  Serial.println("Free RAM test");
+  Serial.println("time_ms,upstream_Pa,downstream_Pa,deltaP_raw_Pa,deltaP_corrected_Pa");
 }
 
 void loop() {
-  Serial.println("Banner demo");
-  display.clearBuffer();
-  display.setTextSize(3);
-  display.setCursor((display.width() - 144) / 2, (display.height() - 24) / 2);
-  display.setTextColor(EPD_BLACK);
-  display.print("Tri");
-  display.setTextColor(EPD_RED);
-  display.print("Color");
-  display.display();
+  float upstream_Pa = readUpstreamPa();
+  float downstream_Pa = readDownstreamPa();
 
-  delay(15000);
+  float deltaP_raw_Pa = upstream_Pa - downstream_Pa;
+  float deltaP_corrected_Pa = deltaP_raw_Pa - zeroOffset_Pa;
 
-  Serial.println("Color rectangle demo");
-  display.clearBuffer();
-  display.fillRect(display.width() / 3, 0, display.width() / 3,
-                   display.height(), EPD_BLACK);
-  display.fillRect((display.width() * 2) / 3, 0, display.width() / 3,
-                   display.height(), EPD_RED);
-  display.display();
+  Serial.print(millis());
+  Serial.print(",");
+  Serial.print(upstream_Pa, 2);
+  Serial.print(",");
+  Serial.print(downstream_Pa, 2);
+  Serial.print(",");
+  Serial.print(deltaP_raw_Pa, 2);
+  Serial.print(",");
+  Serial.println(deltaP_corrected_Pa, 2);
 
-  delay(15000);
-
-  Serial.println("Skipping Text demo");
-  // large block of text
-  display.clearBuffer();
-  display.setTextSize(1);
-  // testdrawtext(
-  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur "
-  //     "adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, "
-  //     "fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor "
-  //     "neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet "
-  //     "ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a "
-  //     "tortor imperdiet posuere. ",
-  //     EPD_BLACK);
-  display.display();
-
-  delay(15000);
-
-  display.clearBuffer();
-  for (int16_t i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, 0, i, display.height() - 1, EPD_BLACK);
-  }
-
-  for (int16_t i = 0; i < display.height(); i += 4) {
-    display.drawLine(display.width() - 1, 0, 0, i, EPD_RED);
-  }
-  display.display();
-
-  delay(15000);
-
-  
-  // Serial.println("Drawing");
-
-  // display.clearBuffer();
-
-  // Serial.println("Buffer cleared");
-
-  // display.fillScreen(EPD_RED);
-
-  // display.display();
-
-  // Serial.println("Done");
-
-  // while (1);
-}
-
-void testdrawtext(const char *text, uint16_t color) {
-  display.setCursor(0, 0);
-  display.setTextColor(color);
-  display.setTextWrap(true);
-  display.print(text);
+  delay(50);
 }
