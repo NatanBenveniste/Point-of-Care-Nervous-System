801aa3c1da0dd802e7cc4432bd132bde73577d2c
johnyoo12 <seongeunyoo1@gmail.com>
2026-05-28 18:35:01 -0700

    bp cuff 2 main functions for prelim design

--- SequencePOC/lib/bpcuff/bpcuff.cpp
+++ SequencePOC/lib/bpcuff/bpcuff.cpp
@@ -77,4 +77,109 @@ void CuffControl::serialControl() {
     CuffControl::valvePulse(100);
     Serial.println("ACK,pulse");
   }
+    else if (cmd == "bp") {
+    CuffControl::runBPMeasurement();
+    Serial.println("ACK,bp");
+  }
+
+  else if (cmd == "hold5") {
+    CuffControl::runFiveMinuteHold();
+    Serial.println("ACK,hold5");
+  }
+
+  else if (cmd == "deflate") {
+    CuffControl::fullDeflate();
+    Serial.println("ACK,deflate");
+  }
 }
+//sequence 1: BP cuff measurement
+void CuffControl::runBPMeasurement() {
+  Serial.println("BP,start");
+
+  //close valve so cuff can inflate
+  CuffControl::valveClose();
+
+  //start pump
+  CuffControl::pumpForward();
+  Serial.println("BP,pumping");
+
+  //inflate for set time
+  //change this number after testing
+  delay(15000);
+
+  //stop pump
+  CuffControl::stopPump();
+  Serial.println("BP,pump_stop");
+
+  //small wait before deflating
+  delay(1000);
+
+  //open valve to slowly deflate
+  CuffControl::valveOpen();
+  Serial.println("BP,deflating");
+
+  //this is where measurement/recording would happen
+  //change this number based on how long deflation takes
+  delay(30000);
+
+  //close valve after measurement
+  CuffControl::valveClose();
+
+  Serial.println("BP,done");
+}
+
+
+//sequence 2: inflate cuff, hold 5 minutes, then deflate
+void CuffControl::runFiveMinuteHold() {
+  Serial.println("HOLD,start");
+
+  //close valve so cuff can inflate
+  CuffControl::valveClose();
+
+  //start pump
+  CuffControl::pumpForward();
+  Serial.println("HOLD,pumping");
+
+  //inflate for set time
+  //change this number after testing
+  delay(15000);
+
+  //stop pump
+  CuffControl::stopPump();
+  Serial.println("HOLD,pump_stop");
+
+  //keep valve closed and hold pressure
+  CuffControl::valveClose();
+  Serial.println("HOLD,holding_5_minutes");
+
+  //hold for 5 minutes
+  delay(300000);
+
+  //deflate cuff
+  Serial.println("HOLD,deflating");
+  CuffControl::valveOpen();
+
+  //leave valve open long enough to fully deflate
+  delay(30000);
+
+  //close valve after deflation
+  CuffControl::valveClose();
+
+  Serial.println("HOLD,done");
+}
+
+
+//manual full deflate sequence
+void CuffControl::fullDeflate() {
+  Serial.println("DEFLATE,start");
+
+  CuffControl::stopPump();
+  CuffControl::valveOpen();
+
+  delay(30000);
+
+  CuffControl::valveClose();
+
+  Serial.println("DEFLATE,done");
+}
+
--- SequencePOC/lib/bpcuff/bpcuff.h
+++ SequencePOC/lib/bpcuff/bpcuff.h
@@ -1,16 +1,23 @@
 #include <Arduino.h>
+#include <Adafruit_LPS35HW.h>
+#pragma once
 
-//define pins
+// define control pins
 #define IN1 3
 #define IN2 2
 #define EEP 4
 #define VALVE 1
 
+// define sensor pins (SPI)
+#define LPS_CS   5
+#define LPS_SCK  28
+#define LPS_MISO 16
+#define LPS_MOSI 19
 
 class CuffControl {
 public:
     CuffControl();
-    
+
     void pumpForward();
     void stopPump();
 
@@ -21,4 +28,8 @@ public:
     void setupHardware();
 
     void serialControl();
+
+    void runBPMeasurement();
+    void runFiveMinuteHold();
+    void fullDeflate();
 };
